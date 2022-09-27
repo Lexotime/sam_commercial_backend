42311e0a6facc36b09a5e5ba5c48bb9a36f22e08
elmoise <coulibalyelhadjimoussa1@gmail.com>
2022-09-28 00:52:00 +0200

    prismaservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './external-service/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports:[ConfigModule.forRoot({isGlobal:true}),PrismaModule],
 })
 export class AppModule {}
