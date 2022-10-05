05cec07aa4d05ea288c44d2ed1aa4d12d1e12e72
Alexandre Adiouma Ndene <alexandreadiouma.ndene@wedocloud.fr>
2022-10-05 14:42:12 +0000

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('Documentation API SAM')
+    .setDescription('You can see here all endpoints you need during yours integrations')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
