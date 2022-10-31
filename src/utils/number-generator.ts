543902c578d0d9e1028571d2b7faa17ff5b68354
Alexandre Adiouma Ndene <alexandreadiouma.ndene@wedocloud.fr>
2022-10-31 18:15:52 +0000

    floor number

--- src/utils/number-generator.ts
+++ src/utils/number-generator.ts
@@ -2,7 +2,7 @@ export const numberArticleGenerator = () => {
     // 4 numbers ----
     let articleNumber = 'ART-';
     for(let i=1; i <= 4; i++)
-        articleNumber = articleNumber + Math.random()*10;
+        articleNumber = articleNumber + Math.floor(Math.random()*10);
     return articleNumber;
 }
 
@@ -10,7 +10,7 @@ export const numberOrderGenerator = () => {
     // 5 numbers ----
     let orderNumber = 'CM-';
     for(let i=1; i <=5; i++)
-        orderNumber = orderNumber + Math.random()*10;
+        orderNumber = orderNumber + Math.floor(Math.random()*10);
     return orderNumber;
 }
 
@@ -18,14 +18,14 @@ export const numberLivraisonGenerator = () => {
     // 5 numbers ----
     let livraisonNumber = 'LV-';
     for(let i=1; i <=5; i++)
-        livraisonNumber = livraisonNumber + Math.random()*10;
+        livraisonNumber = livraisonNumber + Math.floor(Math.random()*10);
     return livraisonNumber;
 }
 
 export const numberFactureGenerator = () => {
     let numberFacture = 'FA-';
     for(let i=1; i<=5; i++)
-        numberFacture = numberFacture + Math.random()*10;
+        numberFacture = numberFacture + Math.floor(Math.random()*10);
     return numberFacture;
 }
 
@@ -33,6 +33,6 @@ export const numberClientGenerator = () => {
     // 3 numbers ----
     let clientNumber = 'CL-';
     for(let i=1; i <=3; i++)
-        clientNumber = clientNumber + Math.random()*10;
+        clientNumber = clientNumber + Math.floor(Math.random()*10);
     return clientNumber;
 }
