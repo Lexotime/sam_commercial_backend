cbd2855d2d3ff2e15a7ccda848eef66dbb10583f
elmoise <coulibalyelhadjimoussa1@gmail.com>
2023-01-13 05:55:26 +0100

    merci

--- src/api/article/article.service.ts
+++ src/api/article/article.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { PrismaService } from 'src/external-service/prisma/prisma.service';
+import { numberArticleGenerator } from 'src/utils/number-generator';
 
 
 @Injectable()
@@ -20,6 +21,16 @@ export class ArticleService {
   async createmany(data : any) {
     // init article.service.create
     for (let i=0;i<data.length;i++){
+      let test;
+      do{
+        data[i].numeroArticle=numberArticleGenerator()
+        test=await this.prismaService.article.findUnique({
+          where:{
+            numeroArticle:data[i].numeroArticle
+          }
+        })
+      }
+      while(test);
       await this.create(data[i])
       
     }
